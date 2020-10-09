7bf5bc5fe46beaf0f69f2d0348120bc195cba838
Kyle West <kyle@familysearch.org>
2020-10-09 06:43:11 -0600

    [init] prepare the repo for npm

---
+++ test/observable-object.test.js
@@ -0,0 +1,3 @@
+test.todo('changing a dep=1 value on an ObservableObject fires the observer')
+test.todo('changing a dep=x value on an ObservableObject fires the observer')
+test.todo('pushing a value to an ObservableObject<array> fires the observer')
---
+++ test/pojo-stick.test.js
@@ -0,0 +1,4 @@
+test.todo('pojoStick() with no args should throw')
+test.todo('pojoStick(<storage-path>) should return an empty object')
+test.todo('pojoStick(<storage-path>, <initialData>) should return a copy of the initial data param')
+test.todo('changing the returned object should reflect to filesystem')
